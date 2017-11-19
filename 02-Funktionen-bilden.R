060f42cbcdf13a3151e1913fd180d397083fa536
W-Ing <w@ingrisch-nea.de>
2017-11-19 19:54:54 +0100

    Naechste Quellen

---
+++ 02-Funktionen-bilden.R
@@ -0,0 +1,28 @@
+
+is_even <- function(x) x %% 2 == 0
+
+mymedian <- function(x){ 
+  l=length(x)
+  #if (l==0) {cat("Fehler: leere Menge \n")}
+  if (l>0){
+      if (is_even(l)) {length(x) <- l-1 }
+      median(x)}
+  else {
+      0}
+}                                             # implementierter Median aus R ist bei geraden Anzahlen unbrauchbar
+
+min_144      <- rollify(min,    window = 144,na_val= 0 )    # Min/Max ueber Fensterbreiten von 144 = 1/2 Tag bilden 
+max_144      <- rollify(max,    window = 144,na_val= 0 )
+
+glaettebreite<- 3
+schieben     <- floor(glaettebreite/2)        # wird spaeter benötigt
+rollmed      <- rollify(.f = ~median(.x,na_val= TRUE), window = glaettebreite  ) # bei Breite 3 keine Vorsicht bzl Median noetig
+
+# Entladung und LAdung auf die zeitlichen Zwischenräume umverteilen
+# dazu werden jeweils die halben Werte die recht und links von einem 5min Zeitraum 
+# registriert sind addiert und
+# als Konvention am ENDE der 5min Phase gespeichert
+#  EHER NICHT NUTZEN
+in_out_pd_mitteln <- rollify(.f = ~sum(.x)/2, window = 2, na_value = NULL)
+
+# 
---
+++ 03-Files-einlesen.R
@@ -0,0 +1,49 @@
+# R-Code fuer Input # library(zoo)
+data <- tibble()
+data0 <- tibble()
+path_to_data <- "./Daten/"
+files <- dir(path=path_to_data, pattern=glob2rx('^Daten_*_11_2017.csv|^Daten_*_10_2017.csv'))  # ^Daten_*_09_2017.csv| |'^Daten_*_11_2017.csv'
+files
+for (file in files)
+{    file=paste(path_to_data,file,sep='',collapse='')
+                    data0 <- read_delim(file, ';',
+                    skip= 2 ,    #  Ignoriere Ueberschrift und leere Datenzeile
+                    col_names = c('zeit',
+                                  'leistung.pv', 
+                                  'leistung.stp', 
+                                  'netzeinspeisung',
+                                  'netzbezug',
+                                  'batt_ladung',
+                                  'batt_entladung',
+                                  'ladezustand'),
+                    col_types =   cols(zeit = col_datetime("%d.%m.%Y %H:%M"),
+                                       leistung.pv = col_integer(),
+                                       leistung.stp = col_integer(),                 
+                                       netzeinspeisung = col_integer(),
+                                       netzbezug = col_integer(),
+                                       batt_ladung = col_integer(),
+                                       batt_entladung = col_integer(), #ladezustand = col_number()
+                                       ladezustand = col_character() )
+                    #n_max=50                   
+)
+data <- rbind(data, data0)
+}
+rm(data0)
+
+cat('gelesen:\n',files, '\n')
+
+# Umwandeln des mit Komma geschrieben Prozentwerts in eine numerische Variable und Umrechnung 100 % entsprechen 10000
+
+data$ladezustand <- as.numeric(gsub(",",".",data$ladezustand))*100
+
+loesche <- c("leistung.stp")                # Duplikat entfernen  
+data    <- data %>% select(-one_of(loesche))
+#-------------------------------------------------------------------
+data <- data %>% 
+    arrange(zeit)     # Um die Zeilen korrekt anzuordnen
+
+
+
+
+
+  
