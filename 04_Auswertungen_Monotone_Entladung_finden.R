7fa249624f1271662c1501f0fb2386309f519cd6
W-Ing <w@ingrisch-nea.de>
2017-11-25 15:05:57 +0100

    Bearbeitung Monotonie

--- 04_Auswertungen_Monotone_Entladung_finden.R
+++ 04_Auswertungen_Monotone_Entladung_finden.R
@@ -1,11 +1,34 @@
 # Monotone Abnahme finden
 
-monoton_fall <- function(xdata){
-      xdata <-   xdata %>%  mutate(sd    = ifelse(lead(ladezustand) <= ladezustand, 1, 0))
-      xdata[is.na(xdata)] <- 0
-      xdata <-   xdata %>%  mutate(sj    = ifelse(sd == 1 &lag(sd) == 0, 1, 0))  
-      xdata[is.na(xdata)] <- 0
-      xdata <-   xdata %>%  mutate(cpm   = cumsum(sd*sj)*sd)
-      return(xdata)
-}
+monotonie_ladezustand <- function(xdata){
+  xdata <- xdata %>%  
+    mutate(gr      = (lag(ladezustand) >  ladezustand),
+           ls      = (lag(ladezustand) <  ladezustand),
+           mono    =  as.integer( ifelse(gr,  -1, 0)),
+           mono    =  as.integer( ifelse(ls,   1, mono))) 
+  xdata[is.na(xdata)] <- 0
+  
+  cnull <- nullen_zaehlen_kurz(xdata)
+  
+  temp2 <- xdata %>%
+    mutate(tmp = abs(mono*ct)) %>% 
+    filter(tmp!=0)
+  erste_nicht_null <- min(temp2$tmp)
+  rm(temp2)
+  
+  while(cnull > erste_nicht_null){
+    xdata <- xdata %>%
+      mutate(prev = lag(mono),
+             change_to_zero = (prev != 0) & (mono == 0), 
+             mono = ifelse(change_to_zero, prev, mono))
+    xdata[is.na(xdata)] <- 0
+    cnull <- nullen_zaehlen_kurz(xdata)}
+    xdata    <- xdata %>% select(-one_of(c("prev","change_to_zero","gr","ls")))
+  return(xdata)
+}  
 
+#----------------------------------------------------------------
+
+nullen_zaehlen_kurz <- function(xdata){
+  return(sum(xdata$mono == 0,na.rm = TRUE))
+} 
