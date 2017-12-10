f514229f3e4fac0fd96bc8564134593a225343b2
W-Ing <w@ingrisch-nea.de>
2017-12-10 22:15:24 +0100

    Mit Fehler in
    ##  Ladung und Entladung summieren und mit Ladezustand vergleichen
    
    gespeichert

---
+++ versuch.R
@@ -0,0 +1,20 @@
+list_fu <- function(start, num, delta){
+    x<-c(start)
+    for (i in 1:(num-1 ) )
+         {x<-append(x,delta)}
+    return(cumsum(x))
+}
+x <- list_fu(0.85, 10, 0.01)         
+y <- list_fu(0.65, 20, 0.01)   
+
+prod<-c()
+for (i in list_fu(0.85, 10, 0.01)){
+   for (j in list_fu(0.65, 10, 0.01)){
+      prod <- append(prod,c(i,j))
+  }
+}
+
+M <- matrix(prod,nrow=2,ncol=length(prod)/2)
+Mat_EtaPaare <- as.tibble(t(M))
+
+
