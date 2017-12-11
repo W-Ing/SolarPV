930678f6a1aa4291bd843fbe25c93263fe38eed8
W-Ing <w@ingrisch-nea.de>
2017-12-11 09:34:22 +0100

    Fehler bereinigt

--- versuch.R
+++ versuch.R
@@ -4,12 +4,10 @@ list_fu <- function(start, num, delta){
          {x<-append(x,delta)}
     return(cumsum(x))
 }
-x <- list_fu(0.85, 10, 0.01)         
-y <- list_fu(0.65, 20, 0.01)   
 
 prod<-c()
-for (i in list_fu(0.85, 10, 0.01)){
-   for (j in list_fu(0.65, 10, 0.01)){
+for (i in list_fu(0.75, 25, 0.01)){
+   for (j in list_fu(0.65, 30, 0.01)){
       prod <- append(prod,c(i,j))
   }
 }
